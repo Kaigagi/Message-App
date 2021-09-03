306d9a0d8cfc1a68d9900fae96dfe4875fe9d310
kaigagi <kaigagipro@gmail.com>
2021-09-03 14:49:59 +0700

    create a server

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require("path");
 const express = require("express");
 
 const port = process.env.port || 3000;
-const server = http.createServer();
 
-console.log(path.join())
-console.log("gfdg")
+const server = http.createServer();
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000);
+
+app.get("/",(req,res)=>{
+    res.sendFile("index.html",{root: __dirname+'/views'})
+})
