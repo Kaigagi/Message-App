8d011b4622df474265207651e0026dfe7f956e00
kaigagi <kaigagipro@gmail.com>
2021-09-03 14:27:27 +0700

    initilize the project

---
+++ server.js
@@ -0,0 +1,10 @@
+const socket = require("socket.io");
+const http = require("http");
+const path = require("path");
+const express = require("express");
+
+const port = process.env.port || 3000;
+const server = http.createServer();
+
+console.log(path.join())
+console.log("gfdg")
